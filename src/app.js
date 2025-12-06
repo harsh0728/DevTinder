1f22bf2e8960d6830b8815d705c279578b1994ae
Harsh Kumar <hk.develop07@gmail.com>
2025-12-06 17:55:42 +0530

    fixed cors origin url

--- src/app.js
+++ src/app.js
@@ -6,9 +6,10 @@ const cors=require("cors");
 const app=express();
 
 app.use(cors({
-    origin:"http://localhost:5173/",
+    origin:"http://localhost:5173",
     credentials:true,
 }))
+
 app.use(express.json());
 app.use(cookieParser());
 
