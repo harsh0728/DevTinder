7a8c54c28ea96bc0b631b8c0dd4aa504588a20a5
Harsh Kumar <hk.develop07@gmail.com>
2026-01-15 00:04:13 +0530

    FIX: changed User to user in ref models and updated CLIENT_URL

--- src/models/chat.js
+++ src/models/chat.js
@@ -3,7 +3,7 @@ const mongoose=require("mongoose");
 const messageSchema=new mongoose.Schema({
     senderId:{
         type:mongoose.Schema.Types.ObjectId,
-        ref:"User",
+        ref:"user",
         required:true,
     },
     text:{
@@ -15,7 +15,7 @@ const messageSchema=new mongoose.Schema({
 const chatSchema=new mongoose.Schema({
     participants:[{
         type:mongoose.Schema.Types.ObjectId,
-        ref:"User",
+        ref:"user",
         required:true
     }],
     messages:[messageSchema]
--- src/models/payment.js
+++ src/models/payment.js
@@ -11,7 +11,7 @@ const paymentSchema=new mongoose.Schema({
     },
     userId:{
         type:mongoose.Schema.Types.ObjectId,
-        ref:"User",
+        ref:"user",
         required:true,
     },
     amount:{
